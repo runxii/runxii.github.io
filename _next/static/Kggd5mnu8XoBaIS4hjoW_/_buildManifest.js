555b98766f9a814a874331c1eebc485f6fb6ef18
runxii <runxii@outlook.com>
2025-12-16 00:50:35 +0000

    Updates

--- _next/static/Kggd5mnu8XoBaIS4hjoW_/_buildManifest.js
+++ _next/static/Kggd5mnu8XoBaIS4hjoW_/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/runxii.github.io//_next/:path+",
+        "destination": "/runxii.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
